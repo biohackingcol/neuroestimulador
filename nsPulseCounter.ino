14f05c20ae4df1c0a21e6e3b5aa99e3c235cfdf0
yjmantilla <yjmantilla@gmail.com>
2019-04-24 14:27:24 -0500

    separacion de las funciones de generacion y contador en codigos distintos

---
+++ nsPulseCounter.ino
@@ -0,0 +1,36 @@
+/*
+ * Pulse Count
+ */
+
+
+int detectPin = A0 ; 
+int long time2=0;
+int long time1=0;
+int delta = 0;
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(detectPin,INPUT);
+}
+
+void loop() 
+
+{
+  //time2 = millis();
+  if(analogRead(detectPin) >= HIGH)
+  {
+    time2=millis();
+    delta = time2 - time1;
+    Serial.print("current: ");
+    Serial.print(time2);
+    //Serial.println(time2);
+    Serial.print(" delta: ");
+    Serial.println(delta);
+    time1=time2; //correcion, al igualar a millis no es exactamente el tiempo anterior.
+    //por eso se iguala directamente a time2
+    //la correcion fue a partir de 2ArduinosPeriods1, las medidas de un solo arduino fueron con el codi
+  }
+  
+  //val = analogRead(detectPin);  // read the input pin
+  //Serial.println(val);          // debug value
+}
