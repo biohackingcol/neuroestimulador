cf04e3c3911f75a6a44537e20e8c720dcf51da29
yjmantilla <yjmantilla@gmail.com>
2019-05-04 17:14:23 -0500

    change frequency control

--- nsPulseGenerator/nsPulseGenerator.ino
+++ nsPulseGenerator/nsPulseGenerator.ino
@@ -4,13 +4,18 @@
  * Output pin = digital 9 = PB1 = OC1A.
  */
 
-const float PWM_PERIOD  = 1;     // 1 s
-const float PULSE_WIDTH = 100e-6;  // 100 us
+//For testing use period=1e-3, width=0.5e-3
+//For deployment use period=1, width=100e-6
+float PWM_PERIOD  = 1e-3;     // 1 s
+float PULSE_WIDTH = 0.5e-3;//100e-6;  // 100 us
+int analogPin = 10;
+char a;
 
 /*
  * With a 16 MHz clock and this prescaler, the resolution is 16 us and
  * the maximum period is 1.048576 s.
  * MAX COUNT: 65536 = 2^n_BITSOFTIMER = 2^16
+ * TIMER1 is 16bit timer, the other 2 (timer 0 and 2) are 8bit
  * RESOLUTION = T_CPU * PRESCALER = PRESCALER / F_CPU
  * MAX_PERIOD= MAX_COUNT * RESOLUTION
  * 
@@ -22,17 +27,43 @@ const float F_TIMER = F_CPU / TIMER_PRESCALER;
 void setup()
 {
     // Configure Timer 1.
+    // |= -> (compound bitwise or)
+    // _BV (bit value) is a macro for
+    // DDRB - The Port B Data Direction Register - read/write, basically 1 is output, 0 is input
+    // PORT B is associated with digital pin 8 to 13
+    // PB1 would be pin 1 of port B (0->8, 1->9) i think
     DDRB  |= _BV(PB1);    // set PB1 = OC1A as output
     ICR1   = round(PWM_PERIOD * F_TIMER) -1 ;
     OCR1A  = round(PULSE_WIDTH * F_TIMER) -1 ;
+    // | -> (bitwise or) 
     TCCR1A = _BV(COM1A1)  // non-inverting PWM on OC1A
            | _BV(WGM11);  // fast PWM mode, TOP = ICR1
     TCCR1B = _BV(WGM12)   // ditto
            | _BV(WGM13)   // ditto
            | _BV(CS12);   // clock at F_CPU/256 = 62.5 kHz
+
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() 
 
 {
+
+  while(Serial.available()) 
+    {
+    a = Serial.read();// read the incoming data as string  
+    }
+
+  if (a == '1'){changePeriod(1e-3);a='0';}
+  if (a == '2'){changePeriod(2e-3);a='0';}
+  
+  analogWrite(analogPin, 255);
+}
+
+
+void changePeriod(float newPeriod)
+{
+  ICR1   = round(newPeriod * F_TIMER) -1 ;
+  Serial.print("Period at: ");
+  Serial.println(newPeriod);
 }
