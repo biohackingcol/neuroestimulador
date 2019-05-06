e3dda449520ae8df422a7069cffa500f548d117e
yjmantilla <yjmantilla@gmail.com>
2019-05-06 12:03:54 -0500

    added explanation for the setup of timer1

--- nsPulseGenerator/nsPulseGenerator.ino
+++ nsPulseGenerator/nsPulseGenerator.ino
@@ -21,33 +21,58 @@ char a;
  * 
  * with prescaler = 256-> resolution = 16us, max period = 1s
  */
-const uint16_t TIMER_PRESCALER = 256;
+const uint16_t TIMER_PRESCALER = 256;//notice that to change this you also need to change CSbits of TCCR1B
 const float F_TIMER = F_CPU / TIMER_PRESCALER;
 
 void setup()
 {
     // Configure Timer 1.
     // |= -> (compound bitwise or)
-    // _BV (bit value) is a macro for
-    // DDRB - The Port B Data Direction Register - read/write, basically 1 is output, 0 is input
+    // _BV (bit value) is a macro for making high whatever bit is inside the ( ), in the context of the bits of the register
+    //thus making bitwise or between _BV will set high each particular bit. Other bitwise operations arise more complicated setups.    
+    // DDRB - The Port B Data Direction Register - read/write, basically 1 is output, 0 is input, is 8bit sized
     // PORT B is associated with digital pin 8 to 13
     // PB1 would be pin 1 of port B (0->8, 1->9) i think
+    //note that Pins 9 and 10 are controlled by timer1 only in the UNO
+    //Servo Library uses Timer1. You can’t use PWM on Pin 9, 10 when you use the Servo Library on an Arduino.
+    // we can use tone function for doc feedback because it relies on timer2
     DDRB  |= _BV(PB1);    // set PB1 = OC1A as output
-    ICR1   = round(PWM_PERIOD * F_TIMER) -1 ;
-    OCR1A  = round(PULSE_WIDTH * F_TIMER) -1 ;
+
+    //in the following n would be the number of the timer in general, x the channel
+    //ICRn - Input Capture Register
+    ICR1   = round(PWM_PERIOD * F_TIMER) -1 ;//when count is equal to ICR1 the counter is reset (going back to BOTTOM and SET (ON)) because ICR1 in the mode chosen is set to TOP.
+    //OCRn - Output Compare Register 
+    OCR1A  = round(PULSE_WIDTH * F_TIMER) -1 ;//when count is equal to OCR1A the ouput is set to clear (OFF) because of non-inverting compare output mode
+    
     // | -> (bitwise or) 
-    TCCR1A = _BV(COM1A1)  // non-inverting PWM on OC1A
+    //TCCRnx - Timer/Counter Control Register, these are both 8bit register that control timer behaviour
+    TCCR1A = _BV(COM1A1)  // non-inverting PWM on OC1A Clear OC1A/OC1B on Compare Match, set OC1A/OC1B at BOTTOM (non-inverting mode)
+                          // In non-inverting Compare Output mode, the Output Compare (OC1x) is cleared on the compare match between TCNT1 and OCR1x, and set at BOTTOM.
+                          // That means that when TCNT1 is equal to OCR1A we get OFF, but when we get to the TOP and then resetted to the BOTTOM, at the BOTTOM we get ON. 
            | _BV(WGM11);  // fast PWM mode, TOP = ICR1
-    TCCR1B = _BV(WGM12)   // ditto
-           | _BV(WGM13)   // ditto
-           | _BV(CS12);   // clock at F_CPU/256 = 62.5 kHz
+                          //The counter counts from BOTTOM to TOP then restarts from BOTTOM
+    TCCR1B = _BV(WGM12)   // fast PWM mode, TOP = ICR1
+           | _BV(WGM13)   // fast PWM mode, TOP = ICR1
+           | _BV(CS12);   // clock at F_CPU/256 = 62.5 kHz, these chooses the prescaler
 
     Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+    /* For some reason:
+    Using the ICR1 Register for defining TOP works well when using fixed TOP values. By using
+    ICR1, the OCR1A Register is free to be used for generating a PWM output on OC1A. However,
+    if the base PWM frequency is actively changed (by changing the TOP value), using the OCR1A
+    as TOP is clearly a better choice due to its double buffer feature.
+
+    NOT SURE IS THIS IS USEFUL IN ANYWAY...
+    */
 }
 
 void loop() 
 
 {
+
+  //we need to find a way to set the output on and off
+  //VDAC could be done with a POT?
 
   while(Serial.available()) 
     {
